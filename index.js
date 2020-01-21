179a6d619b85c8e4959328aeaec2ffdd7d5f3185
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-21 11:50:03 +0100

    :sparkles: restart project fix bug git

---
+++ index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const { engine } = require("express-edge");
+const express = require("express");
+
+const app = new express();
+
+app.use(express.static("public"));
+app.use(engine);
+
+app.set("views", `${__dirname}/views`);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.listen(4000, () => {
+  console.log("App listening on port 4000");
+});
