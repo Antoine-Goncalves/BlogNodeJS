7975118b624d31001cbf092a63e83d6f0b39e6ee
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-22 09:25:54 +0100

    :rocket: :fire: add function for create User

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -1,1 +1,7 @@
-module.exports = (req, res) => {};
+const User = require("../database/models/User");
+
+module.exports = (req, res) => {
+  User.create(req.body, (error, user) => {
+    res.redirect("/");
+  });
+};
