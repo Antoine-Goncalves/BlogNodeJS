d6419b3440ad78065ab1fe2faee24923c2b7892a
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-22 10:01:55 +0100

    :bug: fix bug add return

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -3,7 +3,7 @@ const User = require("../database/models/User");
 module.exports = (req, res) => {
   User.create(req.body, (error, user) => {
     if (error) {
-      res.redirect("/auth/register");
+      return res.redirect("/auth/register");
     }
     res.redirect("/");
   });
