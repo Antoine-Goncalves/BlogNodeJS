4582456b1d6b0b11b49b851df5a1443b2567a558
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-22 09:19:00 +0100

    :fire: add method post for store User in index.js

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const fileUpload = require("express-fileupload");
 
 const mongoose = require("mongoose");
 
-const createPostController = require("./controllers/createPost");
 const homePageController = require("./controllers/homePage");
+
+// Post
+
+const createPostController = require("./controllers/createPost");
 const storePostController = require("./controllers/storePost");
 const getPostController = require("./controllers/getPost");
+
+// User
+
 const createUserController = require("./controllers/createUser");
+const storeUserController = require("./controllers/storeUser");
 
 const app = new express();
 
@@ -32,10 +39,17 @@ app.use("/post/store", storePost);
 app.set("views", `${__dirname}/views`);
 
 app.get("/", homePageController);
+
+// Post
+
 app.get("/post/new", createPostController);
 app.post("/post/store", storePostController);
 app.get("/post/:id", getPostController);
+
+// Register
+
 app.get("/auth/register", createUserController);
+app.post("/users/register", storeUserController);
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
