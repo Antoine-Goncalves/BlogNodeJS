19be9c801bdab8a04a020b0631d12ea464ed386c
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-27 11:30:54 +0100

    :fire: add flash for data

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -8,6 +8,7 @@ module.exports = (req, res) => {
       );
 
       req.flash("registrationErrors", registrationErrors);
+      req.flash("data", req.body);
       return res.redirect("/auth/register");
     }
     res.redirect("/");
