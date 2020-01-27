84b743b602eab6af0b959c837ed3e0862d9a1e91
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-27 13:15:59 +0100

    :bug: fix bug

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,7 +1,7 @@
 const Post = require("../database/models/Post");
 
 module.exports = async (req, res) => {
-  const posts = await Post.find({}).populate("user_id");
+  const posts = await Post.find({}).populate("author");
   console.log(posts);
   res.render("index", {
     posts
