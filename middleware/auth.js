56c108692f8bcb8a695ddf5c0bddd33a1aa42353
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-27 11:04:11 +0100

    :zap: add function for auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,13 @@ const User = require("../database/models/User");
 
 module.exports = (req, res, next) => {
   // fetch user from db
+  User.findById(req.session.userId, (error, user) => {
+    if (error || !user) {
+      return res.redirect("/");
+    }
+
+    next();
+  });
   // verify user
   // if user is valide, permit request.
   // else redirect
