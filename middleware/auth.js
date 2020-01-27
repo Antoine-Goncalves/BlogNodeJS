8588bb19e859239705a0804491c6ab5f20ec6f54
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-27 10:55:04 +0100

    :pencil: add comments for visual

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,6 @@
-module.exports = (req, res, next) => {};
+module.exports = (req, res, next) => {
+  // fetch user from db
+  // verify user
+  // if user is valide, permit request.
+  // else redirect
+};
