800fe2415210935f9461f3f23511f9d4ba9edf8c
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-27 11:05:29 +0100

    :fire: add console.log for catch error

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -3,6 +3,7 @@ const User = require("../database/models/User");
 module.exports = (req, res) => {
   User.create(req.body, (error, user) => {
     if (error) {
+      console.log(error);
       return res.redirect("/auth/register");
     }
     res.redirect("/");
