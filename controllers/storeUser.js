057b4d072528daaf0845cec61e8a2ff3f50e826d
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-27 11:10:34 +0100

    :fire: add const for errors registrations

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -3,9 +3,10 @@ const User = require("../database/models/User");
 module.exports = (req, res) => {
   User.create(req.body, (error, user) => {
     if (error) {
-      console.log(
-        Object.keys(error.errors).map(key => error.errors[key].message)
+      const registrationErrors = Object.keys(error.errors).map(
+        key => error.errors[key].message
       );
+
       return res.redirect("/auth/register");
     }
     res.redirect("/");
