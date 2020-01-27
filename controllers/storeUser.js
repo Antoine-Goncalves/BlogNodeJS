1c3a0f69c604302c6372b04f020072b0ea640c77
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-27 11:09:17 +0100

    :fire: change console.log for more precision

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -3,7 +3,9 @@ const User = require("../database/models/User");
 module.exports = (req, res) => {
   User.create(req.body, (error, user) => {
     if (error) {
-      console.log(error);
+      console.log(
+        Object.keys(error.errors).map(key => error.errors[key].message)
+      );
       return res.redirect("/auth/register");
     }
     res.redirect("/");
