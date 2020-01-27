222736c73d50cff1cf5827d281e9ad8ddd027252
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-27 13:11:59 +0100

    :zap: add console.log for see username

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,8 +1,8 @@
 const Post = require("../database/models/Post");
 
 module.exports = async (req, res) => {
-  const posts = await Post.find({});
-
+  const posts = await Post.find({}).populate("user_id");
+  console.log(posts);
   res.render("index", {
     posts
   });
