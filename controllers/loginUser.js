6c65cfd3c53dd7b891d147383a8b67722c9cefd4
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-27 09:11:51 +0100

    :rocket: add comments for visual

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,5 +1,15 @@
 const User = require("../database/models/User");
 
 module.exports = (req, res) => {
+  // try to find the user
+
+  // compare user password.
+
+  // if user password is correct, then, login user.
+
+  // else
+
+  // redirect user back.
+
   res.redirect("/");
 };
